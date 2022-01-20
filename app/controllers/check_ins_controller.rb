50e8bdc2b1600bbce8fbcaf4f48ace75d8e8a7f7
Jeremiah <jsnidar@gmail.com>
2022-01-20 06:41:28 -0600

    Add actions to check_ins controller

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,2 +1,31 @@
 class CheckInsController < ApplicationController
+
+  def create
+    check_in = @current_user.check_ins.create!(check_in_params)
+    render json: check_in, status: :created
+  end
+
+  def destroy 
+    check_in = CheckIn.find(params[:id])
+    check_in.destroy
+    render json: {}
+  end
+
+  def show
+    check_in = CheckIn.find(params[:id])
+    render json: check_in
+  end
+
+  def update
+    check_in = CheckIn.find(params[:id])
+    check_in.update!(check_in_params)
+    render json: check_in
+  end
+
+  private
+
+  def check_in_params
+    params.permit(:date, :weight, :left_arm_measurement, :left_thigh_measurement, :hips, :waist, :chest, :notes)
+  end
+
 end
