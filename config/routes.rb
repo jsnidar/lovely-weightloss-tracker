1103d3e8134cc05cd71543b4b59768a8ce16627f
Jeremiah <jsnidar@gmail.com>
2022-01-20 06:51:39 -0600

    Add routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   
-  resources :users
+  namespace :api do
+    resources :check_ins, only: [:create, :show, :update, :destroy]
+    resources :goals, except: [:new, :edit]
+
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+    patch "/users/:id", to: "users#update"
+    delete "users/:id", to: "users#destroy"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  end
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
